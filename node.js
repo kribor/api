edce28fa7bef8feacb8226710e6d26f964aa4454
kribor <kristofer@libertar.se>
2017-09-27 19:49:12 +0200

    Added Country lookup

--- node.js
+++ node.js
@@ -18,7 +18,34 @@ app.use(function(req, res, next) {
 
 //Send response with the JSON message
 app.get('/', function (req, res) {
-  res.send(JSON.stringify({ message: "Hello World!" }));
+  res.send(JSON.stringify({ message: "Hello Team 1!" }));
+});
+
+app.get('/country-by-address', function (reqIn, resIn) {
+  var address = reqIn.query.query
+
+  var reqOut = require("request");
+
+  //AIzaSyAMmXpXOpEMQ0DLPtbEqkc6pncFMOKAuuA
+  reqOut.get("https://maps.googleapis.com/maps/api/geocode/json?address=" + address + "&key=AIzaSyAMmXpXOpEMQ0DLPtbEqkc6pncFMOKAuuA", function (errOut, resOut, bodyOut) {
+    var country;
+    if (!errOut) {
+        var resultsObj = JSON.parse(bodyOut);
+
+        resultsObj.results[0].address_components.forEach(function(value){
+          if (value.types.indexOf("country") > -1){
+            country = value.short_name;
+          }
+        });
+    }
+    console.log("Country: " + country);
+    if (country != null) {
+      resIn.send(JSON.stringify({ country: country }))
+    } else {
+      resIn.send(JSON.stringify({ error: "XYZ" }));
+    }
+  });
+
 });
 
 app.get('/health', function (req, res) {
@@ -30,6 +57,3 @@ var server = app.listen(8081, function(){
         var port = server.address().port;
         console.log('Server "api" running at http://127.0.0.1:8081/');
     });
-
-
-
